973801a894aca8642626a7c74ac0e6c432161740
deaundrapeek <jingyufanclub@gmail.com>
2017-02-16 17:45:17 -0500

    add scss loader

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react'
 import UserSignUp from './UserSignUp'
 import Login from './Login'
-import logo from '../logo.svg'
+import logo from '../assets/images/koolaid.png'
+
 // import './stylesheets/App.scss'
 
 import User from './User'
@@ -11,15 +12,17 @@ import Gallery from './Gallery'
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>It Do Curate</h2>
+      <div className="container">
+        <div className="App">
+          <div className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h2>It Do Curate</h2>
+          </div>
+          <UserSignUp />
+          <Login />
+          <User />
+          <Gallery/>
         </div>
-        <UserSignUp />
-        <Login />
-        <User />
-        <Gallery/>
       </div>
     )
   }
