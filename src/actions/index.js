59ded6923899ad4eccdfac9ba6e84d30e2d8b2d6
Kevin Wang <kevinwang@Kevins-MacBook-Air.local>
2017-02-17 17:53:49 -0500

    able to add pieces to current gallery

--- src/actions/index.js
+++ src/actions/index.js
@@ -63,10 +63,17 @@ export const destroySession = () => {
 
 export const searchAPI = (perPage) => {
   let response = axios.get(`http://api.harvardartmuseums.org/object?apikey=48d94c00-f18a-11e6-89ba-839d228fa55c&size=${perPage}&hasimage=1`).then(result => result.data.records)
-
   return {
     type: 'API_RESULTS',
     payload: response
   }
+}
 
+
+export const addPieceToDb = (newPiece) => {
+  let response = axios.post(`${URL}/pieces`, newPiece)
+  return {
+    type: 'ADD_PIECE_TO_GALLERY',
+    payload: response
+  }
 }
--- src/reducers/piece-reducer.js
+++ src/reducers/piece-reducer.js
@@ -2,6 +2,8 @@ export default function(state=[], action) {
   switch (action.type) {
     case 'FETCH_PIECE':
       return [...state, action.payload]
+    case 'ADD_PIECE_TO_GALLERY':
+      return [...state, action.payload]
     default:
       return state
   }
